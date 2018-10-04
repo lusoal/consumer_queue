18b2328ba38ab80851f7ff03d900b1a3ec514aaf
Lucas <lucas.duarte@rivendel.com.br>
2018-10-04 18:48:14 -0300

    Adicionado mysql_insert baseado em tabelas que vem do dicionario

--- database/mysql_insert.py
+++ database/mysql_insert.py
@@ -2,9 +2,17 @@ import yaml
 
 configs = yaml.load(open('config.yml'))
 
-#alterar nome da tabela dependendo da origem do arquivo
 def define_querie(value):
-    querie = "INSERT INTO {} VALUES(".format(configs['database']['table_name'])
+    table_name = None
+
+    #verificar em qual tabela ira inserir o dado parseado
+    if "clientes" in value['table_name']:
+        table_name = "clientes"
+    else:
+        table_name = "transacoes"
+    
+    value.pop('table_name', None)
+    querie = "INSERT INTO {} VALUES(".format(table_name)
     for key, value in value.items():
         querie+=str("'{}',".format(value))
     querie+=")"
